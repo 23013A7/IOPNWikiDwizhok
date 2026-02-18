ceae811394dc1fa4c700399c2b6e02da0d7e3978
23013A7 <23013a72010@gmail.com>
2026-02-18 17:31:31 +0300

    Update wiky.inc.php
    Исправлены заголовки переведёно на русский ящыык там сылки подправлены и ещё чтото

--- wiky.inc.php
+++ wiky.inc.php
@@ -1,8 +1,8 @@
 <?php
-/* Wiky.php - A tiny PHP "library" to convert Wiki Markup language to HTML
- * Author: Toni Lähdekorpi <toni@lygon.net>
+/* Wiky.php - крошечная PHP-библиотека для преобразования вики-разметки в HTML
+ * Автор: Тони Ляхдекорпи <toni@lygon.net>
  *
- * Code usage under any of these licenses:
+ * Использование кода разрешено на условиях любой из следующих лицензий:
  * Apache License 2.0, http://www.apache.org/licenses/LICENSE-2.0
  * Mozilla Public License 1.1, http://www.mozilla.org/MPL/1.1/
  * GNU Lesser General Public License 3.0, http://www.gnu.org/licenses/lgpl-3.0.html
@@ -17,88 +17,94 @@ class wiky {
 		$this->patterns=array(
 			"/\r\n/",
 			
-			// Headings
-			"/^==== (.+?) ====$/m",						// Subsubheading
-			"/^=== (.+?) ===$/m",						// Subheading
-			"/^== (.+?) ==$/m",						// Heading
+			// Заголовки
+			"/^==== (.+?) ====$/m",						// Подподзаголовок
+			"/^=== (.+?) ===$/m",							// Подзаголовок
+			"/^== (.+?) ==$/m",							// Заголовок
+			"/^= (.+?) =$/m",							// Название
 	
-			// Formatting
-			"/\'\'\'\'\'(.+?)\'\'\'\'\'/s",					// Bold-italic
-			"/\'\'\'(.+?)\'\'\'/s",						// Bold
-			"/\'\'(.+?)\'\'/s",						// Italic
+			// Форматирование
+			"/\'\'\'\'\'(.+?)\'\'\'\'\'/s",					// Жирный курсив
+			"/\'\'\'(.+?)\'\'\'/s",							// Жирный
+			"/\'\'(.+?)\'\'/s",							// Курсив
 	
-			// Special
-			"/^----+(\s*)$/m",						// Horizontal line
-			"/\[\[(file|img):((ht|f)tp(s?):\/\/(.+?))( (.+))*\]\]/i",	// (File|img):(http|https|ftp) aka image
-			"/\[((news|(ht|f)tp(s?)|irc):\/\/(.+?))( (.+))\]/i",		// Other urls with text
-			"/\[((news|(ht|f)tp(s?)|irc):\/\/(.+?))\]/i",			// Other urls without text
+			// Специальные элементы
+			"/^----+(\s*)$/m",						// Горизонтальная линия
+			"/\[\[(file|img):((ht|f)tp(s?):\/\/(.+?))( (.+))*\]\]/i",	// (file|img):(http|https|ftp) — изображение
+			"/\[((news|(ht|f)tp(s?)|irc):\/\/(.+?))( (.+))\]/i",		// Другие URL с текстом
+			"/\[((news|(ht|f)tp(s?)|irc):\/\/(.+?))\]/i",				  // Другие URL без текста
+			"/\[\[([^|\]]+)\|([^\]]+)\]\]/",			                        // Внутренние ссылки с текстом
+			"/\[\[([^\]]+)\]\]/",			                                         // Внутренние ссылки без текста
 	
-			// Indentations
-			"/[\n\r]: *.+([\n\r]:+.+)*/",					// Indentation first pass
-			"/^:(?!:) *(.+)$/m",						// Indentation second pass
-			"/([\n\r]:: *.+)+/",						// Subindentation first pass
-			"/^:: *(.+)$/m",						// Subindentation second pass
+			// Отступы
+			"/[\n\r]: *.+([\n\r]:+.+)*/",					// Отступы, первый проход
+			"/^:(?!:) *(.+)$/m",						// Отступы, второй проход
+			"/([\n\r]:: *.+)+/",						// Вложенные отступы, первый проход
+			"/^:: *(.+)$/m",						// Вложенные отступы, второй проход
 	
-			// Ordered list
-			"/[\n\r]?#.+([\n|\r]#.+)+/",					// First pass, finding all blocks
-			"/[\n\r]#(?!#) *(.+)(([\n\r]#{2,}.+)+)/",			// List item with sub items of 2 or more
-			"/[\n\r]#{2}(?!#) *(.+)(([\n\r]#{3,}.+)+)/",			// List item with sub items of 3 or more
-			"/[\n\r]#{3}(?!#) *(.+)(([\n\r]#{4,}.+)+)/",			// List item with sub items of 4 or more
+			// Нумерованный список
+			"/[\n\r]?#.+([\n|\r]#.+)+/",					// Первый проход, поиск всех блоков
+			"/[\n\r]#(?!#) *(.+)(([\n\r]#{2,}.+)+)/",			// Элемент списка с подэлементами уровня 2 и более
+			"/[\n\r]#{2}(?!#) *(.+)(([\n\r]#{3,}.+)+)/",			// Элемент списка с подэлементами уровня 3 и более
+			"/[\n\r]#{3}(?!#) *(.+)(([\n\r]#{4,}.+)+)/",			// Элемент списка с подэлементами уровня 4 и более
 	
-			// Unordered list
-			"/[\n\r]?\*.+([\n|\r]\*.+)+/",					// First pass, finding all blocks
-			"/[\n\r]\*(?!\*) *(.+)(([\n\r]\*{2,}.+)+)/",			// List item with sub items of 2 or more
-			"/[\n\r]\*{2}(?!\*) *(.+)(([\n\r]\*{3,}.+)+)/",			// List item with sub items of 3 or more
-			"/[\n\r]\*{3}(?!\*) *(.+)(([\n\r]\*{4,}.+)+)/",			// List item with sub items of 4 or more
+			// Ненумерованный список
+			"/[\n\r]?\*.+([\n|\r]\*.+)+/",					// Первый проход, поиск всех блоков
+			"/[\n\r]\*(?!\*) *(.+)(([\n\r]\*{2,}.+)+)/",			// Элемент списка с подэлементами уровня 2 и более
+			"/[\n\r]\*{2}(?!\*) *(.+)(([\n\r]\*{3,}.+)+)/",		// Элемент списка с подэлементами уровня 3 и более
+			"/[\n\r]\*{3}(?!\*) *(.+)(([\n\r]\*{4,}.+)+)/",		// Элемент списка с подэлементами уровня 4 и более
 	
-			// List items
-			"/^[#\*]+ *(.+)$/m",						// Wraps all list items to <li/>
+			// Элементы списков
+			"/^[#\*]+ *(.+)$/m",						// Оборачивает все элементы списка в <li/>
 	
-			// Newlines (TODO: make it smarter and so that it groupd paragraphs)
-			"/^(?!<li|dd).+(?=(<a|strong|em|img)).+$/mi",			// Ones with breakable elements (TODO: Fix this crap, the li|dd comparison here is just stupid)
-			"/^[^><\n\r]+$/m",						// Ones with no elements
+			// Переносы строк (TODO: сделать умнее, чтобы группировались в абзацы)
+			"/^(?!<li|dd).+(?=(<a|strong|em|img)).+$/mi",			// Строки с разрываемыми элементами (TODO: исправить эту фигню, сравнение с li|dd здесь глупое)
+			"/^[^><\n\r]+$/m",						// Строки без элементов
 		);
 		$this->replacements=array(
 			"\n",
 			
-			// Headings
+			// Заголовки
+			"<h4>$1</h4>",
 			"<h3>$1</h3>",
 			"<h2>$1</h2>",
 			"<h1>$1</h1>",
 	
-			//Formatting
+			// Форматирование
 			"<strong><em>$1</em></strong>",
 			"<strong>$1</strong>",
 			"<em>$1</em>",
 	
-			// Special
+			// Специальные элементы
 			"<hr/>",
 			"<img src=\"$2\" alt=\"$6\"/>",
 			"<a href=\"$1\">$7</a>",
 			"<a href=\"$1\">$1</a>",
+			"<a href=\"?Page=$1\">$2</a>",
+			"<a href=\"?Page=$1\">$1</a>",
 	
-			// Indentations
-			"\n<dl>$0\n</dl>", // Newline is here to make the second pass easier
+			// Отступы
+			"\n<dl>$0\n</dl>", // Перевод строки здесь нужен для упрощения второго прохода
 			"<dd>$1</dd>",
 			"\n<dd><dl>$0\n</dl></dd>",
 			"<dd>$1</dd>",
 	
-			// Ordered list
+			// Нумерованный список
 			"\n<ol>\n$0\n</ol>",
 			"\n<li>$1\n<ol>$2\n</ol>\n</li>",
 			"\n<li>$1\n<ol>$2\n</ol>\n</li>",
 			"\n<li>$1\n<ol>$2\n</ol>\n</li>",
 	
-			// Unordered list
+			// Ненумерованный список
 			"\n<ul>\n$0\n</ul>",
 			"\n<li>$1\n<ul>$2\n</ul>\n</li>",
 			"\n<li>$1\n<ul>$2\n</ul>\n</li>",
 			"\n<li>$1\n<ul>$2\n</ul>\n</li>",
 	
-			// List items
+			// Элементы списков
 			"<li>$1</li>",
 	
-			// Newlines
+			// Переносы строк
 			"$0<br/>",
 			"$0<br/>",
 		);
