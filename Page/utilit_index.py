2185a890fcfbdcf5cc6f39b75bfc4c011c5d7249
23013A7 <23013a72010@gmail.com>
2026-02-19 13:53:48 +0300

    Обновления utilit_index.py

--- Page/utilit_index.py
+++ Page/utilit_index.py
@@ -2,22 +2,20 @@ import os
 import sys
 
 def main():
-    # Используем текущую папку или переданный путь
     root_dir = os.getcwd() if len(sys.argv) < 2 else sys.argv[1]
 
-    # Папки для поиска
-    subdirs = ['Pages', 'Save']
+    subdirs = ['Pages', 'Save'] # папки для индексации
     output_file = os.path.join(root_dir, 'index.txt')
 
-    pages = set()  # множество, чтобы избежать дубликатов
+    pages = set()
 
     for sub in subdirs:
         target_dir = os.path.join(root_dir, sub)
         if not os.path.isdir(target_dir):
-            print(f"Предупреждение: папка {target_dir} не найдена, пропускаем.")
+            print(f"Ошибка: папка {target_dir} несуществутеэ")
             continue
 
-        # Рекурсивный обход (включает вложенные папки)
+        #тут рекурсия обход и тд в внутрии папок 
         for dirpath, _, filenames in os.walk(target_dir):
             for filename in filenames:
                 if filename.endswith('.iopnwiki'):
