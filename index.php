a3ad67dc38197ac5b93ae640587deee10cee5be0
23013A7 <23013a72010@gmail.com>
2026-02-19 16:21:13 +0300

    Исправлена ошибка с меню у латекс формул

--- index.php
+++ index.php
@@ -46,8 +46,11 @@
     <script>
 MathJax = {
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']]   // <- включает одиночные доллары
+    inlineMath: [['$', '$'], ['\\(', '\\)']]   // включает одиночные доллары
   }
+  options: {
+                enableMenu: false
+            },
 };
     </script>
     <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
