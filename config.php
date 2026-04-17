59b918cc697ac850ac828db5fb462852e7902e8d
23013A7 <23013a72010@gmail.com>
2026-04-17 16:30:56 +0300

    конфиг

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'WikiName'                  => 'ИОПН — Энциклопедия',
+    'WikiSkin'                    => 'iopnwiki',
+    'WikiLanguage'          => 'ru_RU',
+    'WikiFeileIOPNWikiVersion' => 3
+];
