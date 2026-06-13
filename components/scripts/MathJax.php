b8f64596ade3fea8523e6351422c46ca3a460081
23013A7 <23013a72010@gmail.com>
2026-06-13 18:16:24 +0300

    Create MathJax.php
    компонент макх йакс для рендера формул

---
+++ components/scripts/MathJax.php
@@ -0,0 +1,13 @@
+<script>
+    MathJax = {
+        tex: {
+            inlineMath: [['$', '$'], ['\\(', '\\)']], // включает одиночные доллары
+            displayMath: [['$$', '$$'], ['\\[', '\\]']]
+        },
+        options: {
+            enableMenu: false
+        },
+        svg: { fontCache: 'global' }
+    };
+</script>
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
