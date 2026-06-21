db7a64f91a400abeacba64ad96369d025b5129e4
23013A7 <23013a72010@gmail.com>
2026-06-21 10:22:59 +0300

    Добавлен плагин для заголовков
    Добавлен плагин для поддержки заголовков от 1 до 6 уравня

---
+++ 10_Headings.php
@@ -0,0 +1,10 @@
+<?php
+HookManager::register('parse_text', function($text) {
+    $text = preg_replace('/^====== (.+?) ======$/m', '<h6>$1</h6>', $text);
+    $text = preg_replace('/^===== (.+?) =====$/m', '<h5>$1</h5>', $text);
+    $text = preg_replace('/^==== (.+?) ====$/m', '<h4>$1</h4>', $text);
+    $text = preg_replace('/^=== (.+?) ===$/m',   '<h3>$1</h3>', $text);
+    $text = preg_replace('/^== (.+?) ==$/m',     '<h2>$1</h2>', $text);
+    $text = preg_replace('/^= (.+?) =$/m',       '<h1>$1</h1>', $text);
+    return $text;
+}, 10);
