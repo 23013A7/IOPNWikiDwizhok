5044b5cca3e02ffaecbb4e52518b8388aaf6d4b8
23013A7 <23013a72010@gmail.com>
2026-06-22 09:13:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 return [
+    'EngineVersion'        => '0.2.1',
     'WikiName'                  => 'ИОПН — Энциклопедия',
-    'WikiSkin'                    => 'iopnwiki',
+    'WikiSkin'                    => 'iopndoc',
+//    'WikiSkin'                    => 'Tyrma',
 //    'WikiSkin'                    => 'AI2010wiki833',
     'WikiLanguage'          => 'ru_RU',
     'WikiFeileIOPNWikiVersion' => 3
