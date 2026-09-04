63f6fbcffb7fe1448ca521e8dce9640c903e1057
23013A7 <23013a72010@gmail.com>
2026-09-04 14:50:11 +0300

    पार्सर क्लास का परिचय और HTML सैनिटाइज़ेशन
    एक नया Parser क्लास जोड़ा गया है जो Tokenizer, TreeParser और Renderer को एक साथ जोड़ता है। इसमें sanitize विधि शामिल है जो DOMDocument का उपयोग करके अमान्य HTML टैग और विशेषताओं को हटाती है, और javascript: जैसे खतरनाक प्रोटोकॉल को ब्लॉक करती है। साथ ही, पार्सिंग प्रक्रिया के विभिन्न चरणों में प्लगइन्स को डेटा संशोधित करने की अनुमति देने के लिए हुक (hooks) भी जोड़े गए हैं।

--- core/HookManager.php
+++ core/HookManager.php
@@ -1,71 +1,159 @@
 <?php
-class HookManager {
+class Parser {
 
-    private static $hooks = array();
-    private static $errors = array();
+    private $tokenizer;
+    private $treeParser;
+    private $renderer;
 
-    public static function register($hook_name, $callback, $priority = 10) {
-        if (!is_callable($callback)) {
-            self::$errors[] = "HookManager: попытка зарегистрировать не-callable на хук '$hook_name'";
-            return;
+    private $allowedTags = array(
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+        'p', 'br', 'hr',
+        'strong', 'em', 'u', 'sub', 'sup', 's',
+        'ul', 'ol', 'li',
+        'dl', 'dt', 'dd',
+        'a', 'img',
+        'code', 'pre',
+        'div', 'span', 'blockquote',
+        'table', 'thead', 'tbody', 'tfoot',
+        'tr', 'th', 'td',
+        'caption', 'col', 'colgroup',
+        'details', 'summary',
+        'mark',
+    );
+
+    private $allowedAttrs = array(
+        'a'        => array('href', 'title', 'target'),
+        'img'      => array('src', 'alt', 'width', 'height'),
+        'table'    => array('summary'),
+        'th'       => array('colspan', 'rowspan', 'scope'),
+        'td'       => array('colspan', 'rowspan'),
+        'col'      => array('span'),
+        'colgroup' => array('span'),
+        'details'  => array('open'),
+    );
+
+    private $blockedProtocols = array('javascript', 'data', 'vbscript');
+
+    public function __construct() {
+        $this->tokenizer  = new Tokenizer();
+        $this->treeParser = new TreeParser();
+        $this->renderer   = new Renderer();
+
+        $this->allowedTags  = HookManager::apply('parser_allowed_tags',  $this->allowedTags);
+        $this->allowedAttrs = HookManager::apply('parser_allowed_attrs', $this->allowedAttrs);
+
+        foreach ($this->allowedTags as $tag) {
+            if (!isset($this->allowedAttrs[$tag])) {
+                $this->allowedAttrs[$tag] = array();
+            }
+            if (!in_array('style', $this->allowedAttrs[$tag])) {
+                $this->allowedAttrs[$tag][] = 'style';
+            }
+            if (!in_array('class', $this->allowedAttrs[$tag])) {
+                $this->allowedAttrs[$tag][] = 'class';
+            }
         }
-        self::$hooks[$hook_name][$priority][] = $callback;
     }
 
-    public static function apply($hook_name, $value, $data = array()) {
-        if (empty(self::$hooks[$hook_name])) {
-            return $value;
+    public function parse($input) {
+        if (empty($input)) return false;
+
+        $input = str_replace("\r\n", "\n", $input);
+        $input = str_replace("\r", "\n", $input);
+
+        $input = HookManager::apply('parse_before', $input);
+
+        $tokens = $this->tokenizer->tokenize($input);
+
+        $tokens = HookManager::apply('parse_tokens', $tokens);
+
+        $ast = $this->treeParser->build($tokens);
+
+        $ast = HookManager::apply('parse_ast', $ast);
+
+        $html = $this->renderer->render($ast);
+
+        $html = HookManager::apply('parse_after', $html);
+
+        $html = $this->sanitize($html);
+
+        return $html;
+    }
+
+    public function debugAST($input) {
+        $input  = str_replace(array("\r\n", "\r"), "\n", $input);
+        $tokens = $this->tokenizer->tokenize($input);
+        $ast    = $this->treeParser->build($tokens);
+        return $ast->dump();
+    }
+
+    public function debugTokens($input) {
+        $input = str_replace(array("\r\n", "\r"), "\n", $input);
+        return $this->tokenizer->tokenize($input);
+    }
+
+    private function sanitize($html) {
+        if (empty($html)) return $html;
+
+        libxml_use_internal_errors(true);
+        $dom     = new DOMDocument();
+        $wrapped = '<?xml encoding="UTF-8"><div id="parser-root">' . $html . '</div>';
+        @$dom->loadHTML($wrapped, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        libxml_clear_errors();
+
+        $xpath = new DOMXPath($dom);
+        $root  = $dom->getElementById('parser-root');
+        if (!$root) {
+            return htmlspecialchars($html, ENT_QUOTES, 'UTF-8');
         }
 
-        ksort(self::$hooks[$hook_name]);
+        $elements = array();
+        $all      = $xpath->query('.//*', $root);
+        foreach ($all as $el) $elements[] = $el;
+        $elements = array_reverse($elements);
 
-        foreach (self::$hooks[$hook_name] as $priority => $callbacks) {
-            foreach ($callbacks as $callback) {
-                try {
-                    $result = call_user_func($callback, $value, $data);
-                    if ($result !== null) {
-                        $value = $result;
+        foreach ($elements as $el) {
+            $tag = strtolower($el->nodeName);
+
+            if (!in_array($tag, $this->allowedTags)) {
+                $inner    = '';
+                foreach ($el->childNodes as $child) $inner .= $dom->saveHTML($child);
+                $fragment = $dom->createDocumentFragment();
+                @$fragment->appendXML(htmlspecialchars($inner, ENT_QUOTES, 'UTF-8'));
+                $el->parentNode->replaceChild($fragment, $el);
+                continue;
+            }
+
+            if ($el->hasAttributes()) {
+                $remove  = array();
+                $allowed = isset($this->allowedAttrs[$tag]) ? $this->allowedAttrs[$tag] : array();
+
+                foreach ($el->attributes as $attr) {
+                    $name = strtolower($attr->name);
+                    $val  = $attr->value;
+
+                    if (!in_array($name, $allowed)) {
+                        $remove[] = $name;
+                        continue;
                     }
-                } catch (Exception $e) {
-                    self::$errors[] = "Хук '$hook_name' (приоритет $priority): " . $e->getMessage();
-                } catch (Error $e) {
-                    self::$errors[] = "Хук '$hook_name' (приоритет $priority): " . $e->getMessage();
+
+                    if (in_array($name, array('href', 'src', 'action'))) {
+                        $clean = strtolower(trim(preg_replace('/[\x00-\x1F\x7F]/u', '', $val)));
+                        foreach ($this->blockedProtocols as $proto) {
+                            if (strpos($clean, $proto . ':') === 0) {
+                                $remove[] = $name;
+                                break;
+                            }
+                        }
+                    }
                 }
+
+                foreach ($remove as $name) $el->removeAttribute($name);
             }
         }
 
-        return $value;
-    }
-
-    public static function fire($hook_name, $data = array()) {
-        if (empty(self::$hooks[$hook_name])) {
-            return;
-        }
-
-        ksort(self::$hooks[$hook_name]);
-
-        foreach (self::$hooks[$hook_name] as $priority => $callbacks) {
-            foreach ($callbacks as $callback) {
-                try {
-                    call_user_func($callback, $data);
-                } catch (Exception $e) {
-                    self::$errors[] = "Хук '$hook_name' (приоритет $priority): " . $e->getMessage();
-                } catch (Error $e) {
-                    self::$errors[] = "Хук '$hook_name' (приоритет $priority): " . $e->getMessage();
-                }
-            }
-        }
-    }
-
-    public static function has($hook_name) {
-        return !empty(self::$hooks[$hook_name]);
-    }
-
-    public static function getErrors() {
-        return self::$errors;
-    }
-
-    public static function hasErrors() {
-        return !empty(self::$errors);
+        $result = '';
+        foreach ($root->childNodes as $child) $result .= $dom->saveHTML($child);
+        return $result;
     }
 }
