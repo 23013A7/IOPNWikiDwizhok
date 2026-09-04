de0c32b10fe736d310e73ba711c885e87074b1c4
23013A7 <23013a72010@gmail.com>
2026-09-04 16:27:02 +0300

    Zjednodušení vykreslování stránky a delegování speciálních stránek
    Pevně zakódovaný přepínač switch pro zpracování speciálních stránek (jako je indexace nebo seznam všech stránek) byl zcela odstraněn. Místo toho byl přidán podmíněný příkaz, který při aktivním režimu editoru ($editor_mode) načte samostatný soubor render_editor.php a ukončí zpracování skriptu. Zbytek souboru nyní pouze vypíše případné chyby a vygenerovaný wiki obsah ($content_wiki), čímž se logika vykreslování přesouvá na modulárnější systém a pluginy.

--- components/render/render_page.php
+++ components/render/render_page.php
@@ -1,16 +1,9 @@
-<?php echo isset($error) ? $error : ''; echo $content_wiki;?>
 <?php
-    switch($NamePage) {
-        case 'Индексация страниц (служебная)':
-            echo "<p>Привет</p>";
-            require 'indexator.php';
-            break;
-        case 'Все страницы (служебная)':
-            require_once "Page/All_page_table.php";
-            renderArticleTable('Page/index.json');
-            break;
-        case 'Главная страница':
-            require_once "Page/Save/g.php";
-            break;
-    }
+if (isset($editor_mode) && $editor_mode) {
+    require __DIR__ . '/render_editor.php';
+    return;
+}
+
+echo isset($error) ? $error : '';
+echo $content_wiki;
 ?>
